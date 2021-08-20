9c4051001eb6a68b22af19a581905211e299bf1d
MMansour87 <mdj.mansour@gmail.com>
2021-08-20 12:04:17 +0300

    changing class names to match variables names #16

---
+++ script.js
@@ -0,0 +1,20 @@
+// ST of Login section Script -----------------------------------------------
+// Get Elements from DOM
+const intro_right_hand= document.getElementsByClassName('intro_right_hand')[0];
+const intro_left_hand= document.getElementsByClassName('intro-left_hand')[0];
+const intro_vs= document.getElementsByClassName('intro_vs')[0];
+const input_name= document.getElementsByClassName('input_name')[0];
+const error_msg= document.getElementsByClassName('error_msg')[0];
+const login_btn= document.getElementsByClassName('login_btn')[0];
+
+//Get All Sounds
+
+
+
+// Code for Login Animations
+
+
+
+//Code for login Button
+
+// -------------------------------------------------End of Login section Script 
